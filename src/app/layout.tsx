a9897a25ae1d43c07d72d86c8d1a67bb31f8a2c1
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-07-26 11:57:13 +0530

    added comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Providers from "@/components/providers";
+import Providers from "@/components/providers"; //This is where I have all the providers
 
 const inter = Inter({ subsets: ["latin"] });
 
+// Meta is used for seo
 export const metadata: Metadata = {
-  title: "Jil's X Commerce",
+  title: {
+    template: "%s | Jil's X commerce",
+    default: "Jil's X commerce",
+  },
   description: "A good e commerce app",
 };
+
+//This is the main file that will be rendered by Next.js
 
 export default function RootLayout({
   children,
@@ -16,8 +22,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      {/* Inter.className is used when we want this fonts in our entire app, if you want to use the fonts only in some part of the ui try inter.variable
+      @see https://nextjs.org/docs/pages/api-reference/components/font#css-variables
+      */}
       <body className={inter.className}>
+        {/* We are wrapping our entire app including landing page and admin page
+        with Providers */}
         <Providers>{children}</Providers>
       </body>
     </html>
